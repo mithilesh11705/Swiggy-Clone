83a70ba33303321d428f59e60034d07e4a10a622
mithilesh11705 <mithileshdeore7@gmail.com>
2025-02-15 12:57:27 +0530

    Update Body.js

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,26 +1,67 @@
-import RestoCards from "./RestoCards"
+import RestoCards from "./RestoCards";
 import DataList from "../Utils/mockData";
-
-  
-
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 const Body = () => {
-    return (
-      <div className="body">
-        <div className="search">
-          <input type="text" placeholder="Search for Food" />
-          <button>Search</button>
-        </div>
-        <div className="resto-container">
-         {
-          DataList.map((restro) => <RestoCards  
-          key ={restro.info.id}resdata={restro}/>)
-         }
-  
-         
-        </div>
-      </div>
+  const [dataresto, setdataresto] = useState([]);
+
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.9811778&lng=73.7791658&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    console.log(json);
+    setdataresto(
+      json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
     );
   };
-  
 
-  export default Body;
+  //Conditional Rendering
+
+  return dataresto.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="body">
+      <div className="search">
+        <input
+          type="text"
+          className="search-box"
+          placeholder="Search for Food"
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        />
+        <button className="search-btn" onClick={() => {}}>
+          Search
+        </button>
+      </div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = dataresto.filter(
+              (res) => res.info.avgRatingString > 4.3
+            );
+            setdataresto(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="resto-container">
+        {dataresto.map((restro) => (
+          <RestoCards key={restro.info.id} resdata={restro} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
