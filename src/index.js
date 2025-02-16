97ed532d21c4de48cd79085552987067b2940659
mithilesh11705 <mithileshdeore7@gmail.com>
2025-02-16 20:58:03 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import About from "./Components/About";
+import Error from "./Components/Error";
+import Contact from "./Components/Contact";
+import Body from "./Components/Body";
+import Header from "./Components/Header";
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <App />
+      <Outlet />
+    </div>
+  );
+};
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Body/>,
+      },
+      {
+        path: "/About",
+        element: <About />,
+      },
+      {
+        path: "/Contact",
+        element: <Contact />,
+      },
+    ],
+    errorElement: <Error />,
+  },
+]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}>
+      <AppLayout />
+    </RouterProvider>
   </React.StrictMode>
 );
