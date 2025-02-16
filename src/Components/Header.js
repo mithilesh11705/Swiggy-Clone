a60676a7a6a86e4e1888f605854635cbef7696f8
mithilesh11705 <mithileshdeore7@gmail.com>
2025-02-16 20:57:23 +0530

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { LOGO_URL } from "../Utils/constants";
+import { Link } from "react-router-dom";
 const Header = () => {
 
   const [btnName,setbtnName]=useState("Login");
@@ -8,6 +9,7 @@ const Header = () => {
    btnName==="Login"? setbtnName("Logout"):
     setbtnName("Login");
   }
+
 
     return (
       <div className="header">
@@ -19,9 +21,16 @@ const Header = () => {
         </div>
         <div className="Nav-items">
           <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
+            <li>
+            <Link to="/">Home</Link>
+            </li>
+            <li>
+            <Link to="/about">
+            About
+            </Link></li>
+            <li>
+            <Link to="/contact">Contact</Link>
+            </li>
             <li>Cart</li>
             <button className="login-btn" onClick={handleClick}>{btnName}</button>
           </ul>
