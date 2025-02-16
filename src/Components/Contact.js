7709133a4c9332dae5393993bf17c96735f3cfbc
mithilesh11705 <mithileshdeore7@gmail.com>
2025-02-16 21:00:32 +0530

    Add files via upload

---
+++ src/Components/Contact.js
@@ -0,0 +1,17 @@
+const Contact=()=>
+{
+    return (
+        <div className="contact">
+            <h1>Get In touch With Mithilesh Doere</h1>
+            <h1>
+                gmail:mithileshdeore7@gmail.com
+            </h1>
+
+            <h1>
+                Phone: +91 8010309017
+            </h1>
+        </div>
+    );
+}
+
+export default Contact;
---
+++ src/Components/RestroMenu.js
@@ -0,0 +1,14 @@
+const RestroMenu=()=>
+{
+    return (
+        <div className="menu">
+                <h1>Name of the Restaurant</h1>
+                <h2>Menu</h2>
+                <ul>
+                    <li>Biryani</li>
+                    <li>Burgers</li>
+                    <li>Coke</li>
+                </ul>
+        </div>  
+    )
+}
