c1279d7212000c4cc6b89a484abd2b88faf8573b
mithilesh11705 <mithileshdeore7@gmail.com>
2025-02-18 15:53:20 +0530

    Update RestoCards.js

--- src/Components/RestoCards.js
+++ src/Components/RestoCards.js
@@ -1,23 +1,18 @@
 import { CDN_URL } from "../Utils/constants";
 
 const RestoCards = (props) => {
-    const { resdata } = props;
-    return (
-      <div className="card">
-        <img
-          id="li"
-          src={
-            CDN_URL+resdata.info.cloudinaryImageId
-          }
-        />
-        <h2>{resdata.info.name}</h2>
-        <h4 >{resdata.info.cuisines.join(",")}</h4>
-        <h4 >Rating: {resdata.info.avgRatingString}</h4>
-        <h4 className="wrap">Delivery Time: {resdata.info.sla.deliveryTime} Minutes</h4>
-   
-        
-      </div>
-    );
-  };
-  
-  export default RestoCards;
+  const { resdata } = props;
+  return (
+    <div className="card">
+      <img id="li" src={CDN_URL + resdata.info.cloudinaryImageId} />
+      <h2>{resdata.info.name}</h2>
+      <h4>{resdata.info.cuisines.join(",")}</h4>
+      <h4>Rating: {resdata.info.avgRatingString}</h4>
+      <h4 className="wrap">
+        Delivery Time: {resdata.info.sla.deliveryTime} Minutes
+      </h4>
+    </div>
+  );
+};
+
+export default RestoCards;
