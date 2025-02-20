8a50068b3fc52ebfb075f84d76edefe6d7af5dc4
mithilesh11705 <mithileshdeore7@gmail.com>
2025-02-20 22:49:10 +0530

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,42 +2,41 @@ import { useState } from "react";
 import { LOGO_URL } from "../Utils/constants";
 import { Link } from "react-router-dom";
 import useOnlineStatus from "../Utils/useOnlineStatus";
+import Button from "@mui/material/Button";
 const Header = () => {
   const [btnName, setbtnName] = useState("Login");
 
   const handleClick = () => {
     btnName === "Login" ? setbtnName("Logout") : setbtnName("Login");
   };
-  const onlinestatus=useOnlineStatus();
+  const onlinestatus = useOnlineStatus();
   return (
-    <div className="header">
-      <div className="logo">
-        <img id="l" src={LOGO_URL} />
+    <div className="flex justify-between bg-pink-100 shadow-lg m-3 mb-4 ">
+      <div className="w-32">
+        <img className="" src={LOGO_URL} />
       </div>
-      <div className="Nav-items">
-        <ul>
-        <li>
-          Online Status:{onlinestatus ? "✅" : "❌" }
-        </li>
-          <li>
+      <div className=" flex items-center ">
+        <ul className="flex p-6 m-5" >
+          <li className="px-4">Online Status:{onlinestatus ? "✅" : "❌"}</li>
+          <li className="px-4">
             <Link className="link" to="/">
               Home
             </Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link className="link" to="/about">
               About
             </Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link className="link" to="/contact">
               Contact
             </Link>
           </li>
-          <li>Cart</li>
-          <button className="login-btn" onClick={handleClick}>
+          <li className="px-4">Cart</li>
+          <Button className=" mt-3" variant="contained" onClick={handleClick}>
             {btnName}
-          </button>
+          </Button>
         </ul>
       </div>
     </div>
